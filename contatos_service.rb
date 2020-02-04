7ae61e91aa01178e2eb2d9a950ca968cea30e4f5
Dictest85 <52790073+Dictest85@users.noreply.github.com>
2020-02-04 11:04:13 -0300

    Delete contatos_service.rb

--- contatos_service.rb
+++
@@ -1,9 +0,0 @@
-module Contato
-    include HTTParty
-    default_options.update(verify: false) ## - para poder chamar rodar os specs mesmo com restrição de conexão.
-    ## - Deverá ser utilizado com conexão Wifi
-    base_uri 'https://api-de-tarefas.herokuapp.com'
-    format :json
-    headers Accept: 'application/vnd.tasksmanager.v2',
-                    'Content-Type': 'application/json'
-end
