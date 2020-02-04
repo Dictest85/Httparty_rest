2d5375c6bb871bec89c2bdeee2fcc8e5d85fbf14
Dictest85 <52790073+Dictest85@users.noreply.github.com>
2020-02-04 11:18:44 -0300

    Add files via upload

---
+++ features/step_definitions/get.rb
@@ -0,0 +1,14 @@
+#coding:utf-8
+Dado("que eu faça um GET") do
+   #describe 'fazer uma requisicão' do - comando utilizado via rspec 
+end
+  
+Quando("eu executar tipo de comando GET") do
+   #it 'get' do - comando utilizado via rspec
+   #@melhor_opcao = Contato.get('/contacts/') - comando utilizado via rspec
+   @melhor_opcao = HTTParty.get('/contacts/')
+end
+  
+Entao("o retorno trara todos os registros") do
+   puts = @melhor_opcao
+end
